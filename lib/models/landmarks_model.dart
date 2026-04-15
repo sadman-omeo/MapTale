cffbada242b6be09f723781cba77ca8f0a44cb49
SADMAN AHMED OMEO <sadmanahmed167@gmail.com>
2026-04-15 19:18:56 +0600

    models

---
+++ lib/models/landmarks_model.dart
@@ -0,0 +1,25 @@
+
+
+class Landmarks {
+  final int id;
+  final String title;
+  final double lati;
+  final double longi;
+  final String image;
+  final double score;
+  final int visit_count;
+  final double? avg_distance;
+
+  Landmarks({
+    required this.id,
+    required this.title,
+    required this.lati,
+    required this.longi,
+    required this.image,
+    required this.score,
+    required this.visit_count,
+    required this.avg_distance,
+  });
+
+  
+}
